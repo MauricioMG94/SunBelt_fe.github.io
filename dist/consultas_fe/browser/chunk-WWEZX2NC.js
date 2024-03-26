4b466662d625fdb1e46c8b8d64e0fbdb063623ca
Mauricio Martinez <116044954+MauricioMG94@users.noreply.github.com>
2024-03-26 01:58:22 -0500

    Add files via upload

---
+++ dist/consultas_fe/browser/chunk-WWEZX2NC.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-7PLNIC5Z.js";export default a();
---
+++ dist/consultas_fe/server/chunk-X75X3755.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-O46CU6B4.mjs";import"./chunk-VVCT4QZE.mjs";export default a();
